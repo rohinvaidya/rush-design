3e39f89275d7c88d1985d71c63963e782844bdc7
rohinvaidya <rohinvaidya@outlook.com>
2026-05-24 09:21:40 -0400

    Implementation for Assignment 5

--- rush-1-5/rush.py
+++ rush-1-5/rush.py
@@ -3,7 +3,8 @@ import sys
 
 def rush(x, y):
     """
-    Display a square using 'A' top-left, 'B' borders, 'C' bottom-left, with top/bottom rows mirrored horizontally.
+    Display a square using 'A', 'B', 'C' where the bottom row is the horizontal
+    mirror of the top row.
 
     Args:
         x (int): Width of the square
@@ -13,5 +14,20 @@ def rush(x, y):
         print("Invalid size", file=sys.stderr)
         return
 
-    # Your implementation here
-    pass
+    if x == 1:
+        for _ in range(y):
+            print('B')
+        return
+
+    if y == 1:
+        print('B' * x)
+        return
+
+    top = 'A' + 'B' * (x - 2) + 'C'
+    middle = 'B' + ' ' * (x - 2) + 'B'
+    bottom = top[::-1]
+
+    print(top)
+    for _ in range(y - 2):
+        print(middle)
+    print(bottom)
