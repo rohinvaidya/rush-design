37248ec6f1b052ac1fcfef20dd08faef2eebbb31
rohinvaidya <rohinvaidya@outlook.com>
2026-05-24 09:12:48 -0400

    Implementation for Assignment 1

--- rush-1-1/rush.py
+++ rush-1-1/rush.py
@@ -13,5 +13,14 @@ def rush(x, y):
         print("Invalid size", file=sys.stderr)
         return
 
-    # Your implementation here
-    pass
+    if y == 1:
+        print('o' + '-' * (x - 2) + 'o' if x > 1 else 'o')
+        return
+
+    top_bottom = 'o' + '-' * (x - 2) + 'o' if x > 1 else 'o'
+    middle = '|' + ' ' * (x - 2) + '|' if x > 1 else '|'
+
+    print(top_bottom)
+    for _ in range(y - 2):
+        print(middle)
+    print(top_bottom)
